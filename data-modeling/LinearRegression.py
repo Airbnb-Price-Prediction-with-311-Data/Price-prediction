5df5676c3a501ffcd1cd6e374c53b60ea422dc50
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-05-02 17:18:43 -0400

    data modeling updated

--- data-modeling/LinearRegression.py
+++ data-modeling/LinearRegression.py
@@ -7,22 +7,28 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import accuracy_score
 from allcolumns import columns
-airbnb_service_complaints = pd.read_csv("Airbnb_Service_complaints.csv")
 
+airbnb_service_complaints = pd.read_csv("../dataset/Airbnb_Service_complaints.csv")
+f=open("linear_reg.txt", "a+")
 
 def airbnb_allservices():
     numerical_features =  airbnb_service_complaints.select_dtypes(exclude=['object'])
-    #categorical_features = airbnb_service_complaints.select_dtypes(include=['object'])
+    categorical_features = airbnb_service_complaints.select_dtypes(include=['object'])
     y = numerical_features.price
     numerical_features.drop(['price'], axis=1, inplace=True)
-    numerical_features.drop(['count'], axis=1, inplace=True)
+    #categorical_features_one_hot = pd.get_dummies(categorical_features)
+    #X = np.concatenate((numerical_features, categorical_features_one_hot), axis=1)
     X_train, X_test, y_train, y_test = train_test_split(numerical_features, y, test_size=0.2)
     regr = linear_model.LinearRegression()
     regr.fit(X_train, y_train)
     y_pred = regr.predict(X_test)
     print ("LINEAR REGRESSION ON ENTIRE DATASET")
     print ("ERROR")
+    f.writelines("ERROR  \n")
+    f.writelines(str(mean_squared_error(y_test, y_pred)))
     print (mean_squared_error(y_test, y_pred))
+    f.writelines("ACCURACY")
+    f.writelines(str(1- mean_squared_error(y_test, y_pred)))
     print ("ACCURACY")
     print (1- mean_squared_error(y_test, y_pred))
 
@@ -35,30 +41,38 @@ def airbnb_perservice():
     airbnb_subset = airbnb_service_complaints.copy()
     for col in columns:
         airbnb_subset.drop([col], axis=1, inplace=True)
-    y = airbnb_service_complaints.price
-    airbnb_subset.drop(['latitude', 'longitude', 'count', 'total_count', 'price'], axis=1, inplace=True)
-    
+
+    #count = 0
+    print (airbnb_subset.columns)
+    y = airbnb_subset.price
+    airbnb_subset.drop(['price', 'zipcode', 'total_count', 'name', 'host_name'], axis=1, inplace=True)
     for col in columns:
         row = []
-        complaint = airbnb_service_complaints[col]
-        airbnb_subset[col] = complaint
+        #complaint = airbnb_service_complaints[col]
+        #airbnb_subset[col] = complaint
         numerical_features = airbnb_subset.select_dtypes(exclude=['object'])
-        X_train, X_test, y_train, y_test = train_test_split(numerical_features, y, test_size=0.2)
+        categorical_features = airbnb_service_complaints.select_dtypes(include=['object'])
+        categorical_features_one_hot = pd.get_dummies(categorical_features)
+        X = np.concatenate((numerical_features, categorical_features_one_hot), axis=1)
+        print (numerical_features.columns)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        airbnb_subset.drop([col], axis=1, inplace=True)
-        complaints[col] = mean_squared_error(y_test, y_pred)
+        #airbnb_subset.drop([col], axis=1, inplace=True)
+        #complaints[col] = mean_squared_error(y_test, y_pred)
         row.append(col)
         row.append(mean_squared_error(y_test, y_pred))
         row.append(1-mean_squared_error(y_test, y_pred))
         rows.append(row)
+        print (mean_squared_error(y_test, y_pred))
+        print ("ERROR")
+        break
     print (complaints)   
 
-    with open(filename, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(fields)
-        csvwriter.writerows(rows)
-
+    ##with open(filename, 'w') as csvfile:
+    #   csvwriter = csv.writer(csvfile)
+    #    csvwriter.writerow(fields)
+    #    csvwriter.writerows(rows)
 
 airbnb_allservices()
-airbnb_perservice()
+#airbnb_perservice()
