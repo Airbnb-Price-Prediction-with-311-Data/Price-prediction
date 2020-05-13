e0516bfb3fd1061b2e9d3a1afd15df8509378108
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-05-12 21:27:02 -0400

    311 cleaning

--- data-cleaning/311-dataset-cleaning/filter_data_by_date.py
+++ data-cleaning/311-dataset-cleaning/filter_data_by_date.py
@@ -5,9 +5,9 @@ import matplotlib.image as mpimg
 import seaborn as sns
 
 def remove_2020():
-    three11 = pd.read_csv('../Data/311_Service_Requests_from_2010_to_Present.csv')
+    three11 = pd.read_csv('../../dataset/311_Service_Requests_from_2010_to_Present.csv')
     indexNames = three11[ three11['Created_Date'].str.contains('/2020') ].index
     three11.drop(indexNames , inplace=True)
-    three11.to_csv('../Data/311_Service_Requests_from_2010_to_Present.csv', index=True)
+    three11.to_csv('../../dataset/311_Service_Requests_from_2010_to_Present.csv', index=True)
 
 remove_2020()
