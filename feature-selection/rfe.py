4146227b720943399835a5a317f410e11749fbf7
agnithamohan <agnitha.mohan@gmail.com>
2020-05-13 00:37:49 -0400

    rfe

--- feature-selection/rfe.py
+++ feature-selection/rfe.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import RFE
@@ -12,7 +11,7 @@ from sklearn.metrics import mean_squared_error
 
 
 def make_subset(): 
-    airbnb_complaints = pd.read_csv('Price-prediction/dataset/Airbnb_Service_complaints.csv')
+    airbnb_complaints = pd.read_csv('../dataset/Airbnb_Service_complaints.csv')
     numerical_features = airbnb_complaints.select_dtypes(exclude=['object'])
     y = numerical_features.price
     numerical_features.drop(['price'], axis=1, inplace=True)
@@ -24,7 +23,6 @@ def make_subset():
     X_rfe = rfe.fit_transform(X_train,y_train)  
     #Fitting the data to model
     model.fit(X_rfe,y_train)
-    # print(rfe.support_)
     X_test = rfe.fit_transform(X_test,y_test)  
     i = 0
     list = []
@@ -41,7 +39,7 @@ def make_subset():
     for x in airbnb_complaints.columns:
         if(x not in relevant_set):
             airbnb_complaints.drop([x],axis=1,inplace=True)
-    airbnb_complaints.to_csv('../dataset/reduced_data_rfe.csv')
+    airbnb_complaints.to_csv('../dataset/reduced_data_rfe.csv', index=False)
 
 
 
