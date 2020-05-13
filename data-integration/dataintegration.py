fafb70b89ba39a1dff0b9c14cab73f9fd96c0c42
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-05-13 17:49:30 -0400

    integration

---
+++ data-integration/dataintegration.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+airbnb_complaints = pd.read_csv('../dataset/Airbnb_processed2.csv')
+df_complaint_types = pd.read_csv("../dataset/complaint_type.csv")
+for index, row in df_complaint_types.iterrows():
+      df_complaint_types.at[index, 'Incident_Zip'] = str(row['Incident_Zip']).zfill(5)
+df_complaint_types.drop(['Unnamed: 0'], axis=1, inplace=True)
+df_complaint_types.rename(columns = {'Incident_Zip':'zipcode'}, inplace = True) 
+
+df_merge_complaint_types = pd.merge(airbnb_complaints, df_complaint_types, on='zipcode', how='inner')
+df_merge_complaint_types.to_csv(r'../dataset/Airbnb_Service_complaints_merged.csv', index = False)
+
