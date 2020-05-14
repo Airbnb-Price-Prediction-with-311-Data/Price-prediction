b059c510bc49bdcf634d034b7f3451035010df65
Srishti Bhargava <srishti.bhargava1995@gmail.com>
2020-05-14 13:20:41 -0400

    data modeling for all feature selection

--- data-modeling/run_models.py
+++ data-modeling/run_models.py
@@ -2,55 +2,191 @@ import pandas as pd
 import numpy as np
 import sklearn
 import csv
+import xgboost as xgb
+from sklearn import ensemble
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import accuracy_score
 from allcolumns import columns
 
-airbnb_service_complaints = pd.read_csv("../dataset/Airbnb_Service_complaints.csv")
+#airbnb_service_complaints = pd.read_csv("../dataset/Airbnb_Service_complaints.csv")
 f=open("results/errors.csv", "a+")
 
-def airbnb_allservices(airbnb_service_complaints, model ,ft_sel_name, model_name):
-    numerical_features =  airbnb_service_complaints.select_dtypes(exclude=['object'])
+def airbnb_allservices(filename, model ,ft_sel_name, model_name):
+    airbnb_service_complaints = pd.read_csv(filename)
+    numerical_features = airbnb_service_complaints.select_dtypes(exclude=['object'])
     y = numerical_features.price
     numerical_features.drop(['price'], axis=1, inplace=True)
     X_train, X_test, y_train, y_test = train_test_split(numerical_features, y, test_size=0.2)
-    regr = linear_model.LinearRegression()
-    regr.fit(X_train, y_train)
-    y_pred = regr.predict(X_test)
-    # print ("LINEAR REGRESSION ON ENTIRE DATASET")
-    # print ("ERROR")
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     f.writelines(ft_sel_name+","+model_name+","+str(mean_squared_error(y_test, y_pred))+"\n")
 
-
-#airbnb_allservices()
-#Feature Selection - NONE
+#Feature Selection - None
+filename = "../dataset/Airbnb_processed_try_1.csv"
 ft_sel_name = 'None'
+
 model_name = 'Linear Regression'
-# call fn 
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
 
-ft_sel_name = 'None'
 model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
 
-
-ft_sel_name = 'None'
 model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Hand Picking
+filename = "../feature-selection/results/feature_selection_handpicking_v1.csv"
+ft_sel_name = 'Hand-Picking'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Pearson Correlation
+filename = "../feature-selection/results/feature_selection_correlation_v1.csv"
+ft_sel_name = 'Filtering-Pearson Correlation'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
 
 
-ft_sel_name = 'None'
-model_name = 'Knn'
+#Feature Selection - Recursive Feature Elimination
+filename = "../feature-selection/results/reduced_data_rfe_v1.csv"
+ft_sel_name = 'Recursive Feature Elimination'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Forward Selection Intersection
+filename = "../feature-selection/results/forward_selection_intersection_v1.csv"
+ft_sel_name = 'Forward Selection Intersection'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Forward Selection lowest 5 per complaint
+filename = "../feature-selection/results/forward_selection_top5_v1.csv"
+ft_sel_name = 'Forward Selection 5 lowest errors per service'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Forward Selection lowest 10 per complaint
+filename = "../feature-selection/results/forward_selection_top10_v1.csv"
+ft_sel_name = 'Forward Selection 10 lowest errors per service'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+#Feature Selection - Forward Selection lowest 15 per complaint
+filename = "../feature-selection/results/forward_selection_top15_v1.csv"
+ft_sel_name = 'Forward Selection 15 lowest errors per service'
+
+model_name = 'Linear Regression'
+model = linear_model.LinearRegression()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Ensemble'
+model = ensemble.GradientBoostingRegressor()
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'XgBoost'
+model = xgb.XGBRegressor() 
+airbnb_allservices(filename, model, ft_sel_name, model_name)
+
+model_name = 'Random Forest'
+model = RandomForestRegressor(max_depth=2, random_state=0)
+airbnb_allservices(filename, model, ft_sel_name, model_name)
 
 
-ft_sel_name = 'None'
-model_name = 'Random Forest' 
-
-
-
-
-
-
-
+'''
 'None',
 'Random Forest',  0.3542529602630437],
 'Hand-picking', 'Linear Regression', 0.32840295063492975],
@@ -73,98 +209,6 @@ model_name = 'Random Forest'
 'Forward Selection', 'XgBoost', 0.27508785316309353],
 'Forward Selection', 'Knn', 0.3434047632654581],
 'Forward Selection', 'Random Forest', 0.3518038106524232],
- 
-
-
-
-#Feature Selection - RFE
-ft_sel_name = 'Recursive Feature Elimination'
-model_name = 'Linear Regression'
-# call fn 
-
-ft_sel_name = 'Recursive Feature Elimination'
-model_name = 'Ensemble'
-
-
-ft_sel_name = 'Recursive Feature Elimination'
-model_name = 'XgBoost'
-
-
-ft_sel_name = 'Recursive Feature Elimination'
-model_name = 'Knn'
-
-
-ft_sel_name = 'Recursive Feature Elimination'
-model_name = 'Random Forest' 
-
-
-#Feature Selection - Corr
-
-ft_sel_name = 'Filtering-Pearson Correlation'
-model_name = 'Linear Regression'
-# call fn 
-
-ft_sel_name = 'Filtering-Pearson Correlation'
-model_name = 'Ensemble'
-
-
-ft_sel_name = 'Filtering-Pearson Correlation'
-model_name = 'XgBoost'
-
-
-ft_sel_name = 'Filtering-Pearson Correlation'
-model_name = 'Knn'
-
-
-ft_sel_name = 'Filtering-Pearson Correlation'
-model_name = 'Random Forest' 
-
-
-
-#Feature Selection - Fwd sel
-ft_sel_name = 'Forward Selection'
-model_name = 'Linear Regression'
-# call fn 
-
-ft_sel_name = 'Forward Selection'
-model_name = 'Ensemble'
-
-
-ft_sel_name = 'Forward Selection'
-model_name = 'XgBoost'
-
-
-ft_sel_name = 'Forward Selection'
-model_name = 'Knn'
-
-
-ft_sel_name = 'Forward Selection'
-model_name = 'Random Forest' 
-
-
-
-
-#Feature Selection - hand picking 
-
-ft_sel_name = 'Hand-picking'
-model_name = 'Linear Regression'
-# call fn 
-
-ft_sel_name = 'Hand-picking'
-model_name = 'Ensemble'
-
-
-ft_sel_name = 'Hand-picking'
-model_name = 'XgBoost'
-
-
-ft_sel_name = 'Hand-picking'
-model_name = 'Knn'
-
-
-ft_sel_name = 'Hand-picking'
-model_name = 'Random Forest' 
-
-
+'''
 
 
